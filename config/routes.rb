fae8f62d094f52d7ada15eb6d8927bf270fd002c
Mark Clifford <mark.clifford.mail@gmail.com>
2015-04-09 00:03:08 +1200

    Added sessions and admin and a basic page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,31 @@
 Rails.application.routes.draw do
+
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get 'admin/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+
   resources :dogs
+
+  # Setup admin routes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408091627) do
+ActiveRecord::Schema.define(version: 20150408095853) do
 
   create_table "dogs", force: true do |t|
     t.string   "name"
     t.string   "breed"
     t.date     "dob"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
