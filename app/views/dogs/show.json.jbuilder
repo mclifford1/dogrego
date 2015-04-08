219b4032db4ceab3d5157cedfb87b37c11a6f200
Mark Clifford <mark.clifford.mail@gmail.com>
2015-04-08 21:33:10 +1200

    Dog Registration Scaffold

---
+++ app/views/dogs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @dog, :id, :name, :breed, :dob, :created_at, :updated_at
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dog.delete_all
+# . . .
+Dog.create!(name: 'Rover',
+breed: 'Golden Retriever',
+dob: DateTime.strptime("09/14/2009", "%m/%d/%Y"))
+
+Dog.create!(name: 'Fido',
+breed: 'Terrier',
+dob: DateTime.strptime("02/23/2011", "%m/%d/%Y"))
