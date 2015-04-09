41c65a4636e1bbeee30b0c701093c9798fab50f2
Mark Clifford <mark.clifford.mail@gmail.com>
2015-04-10 01:04:09 +1200

    Added working email support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,19 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.live.com",
+    port: 587,
+    domain: "mail.live.com",
+    authentication: "plain",
+    user_name: "jarvissh@hotmail.com",
+    password: "octG3rian$",
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
